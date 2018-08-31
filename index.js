f1f5ca0c78c5f4355f3bdb64a9fbf81cc4477a7c
github username <github email address>
2018-08-31 13:21:40 -0400

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,55 @@ const questions = [
   {questionText: "Goldfish only have a memory of about three seconds", answer: false}
 ]
 
-let question;
+let question
+
+const div = document.querySelector(".question-container")
+
+const appendQuestion = (asked) => { div.innerHTML  = asked.questionText
+}
+
+const askQuestionThen = (time) => {
+  question =  questions[0];
+  appendQuestion(question)
+
+  return new Promise(function(resolve, reject) {
+  setTimeout(resolve, time);
+  });
+}
+
+const removeQuestion = () => {
+  div.innerText = ''
+}
+
+const askQuestionThenRemoveQuestion = (time) => {
+  return askQuestionThen(time)
+  .then(removeQuestion )
+}
+
+const trueAndFalseButtons = () => {
+return document.querySelector('.true-false-list').querySelectorAll('.btn')
+}
+
+const toggleTrueAndFalseButtons = () => {
+
+  if (trueAndFalseButtons()[0].classList.contains("hide")){
+    trueAndFalseButtons().forEach(function(btn){
+     btn.classList.remove('hide')}
+     )
+  }else {
+    trueAndFalseButtons().forEach((btn) => {
+     btn.classList.add('hide')}
+   )
+  }
+}
+
+
+const displayQuestionOnClick = () => {
+  toggleTrueAndFalseButtons();
+  let ask = document.querySelector(".waves-effect")
+  ask.addEventListener("click",
+    askQuestionThenRemoveQuestion(5000),
+ )
+ toggleTrueAndFalseButtons();
+
+}
